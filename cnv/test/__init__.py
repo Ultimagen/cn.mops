7d6e46851637e6f5b1f08aa1d5d6b2796785de62
avigailmo <avigail.moldovan@ultimagen.com>
2024-06-30 12:50:56 +0300

    fix get_resource_dir

--- cnv/test/__init__.py
+++ cnv/test/__init__.py
@@ -5,5 +5,5 @@ from os.path import dirname
 test_dir = dirname(__file__)
 
 def get_resource_dir(module_file: str):
-    test_path = os.path.splitext(module_file)[0].split("/tests/")[1]
+    test_path = os.path.splitext(module_file)[0].split("/test/")[1]
     return f"{test_dir}/resources/{test_path}"
