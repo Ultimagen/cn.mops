5756f15fe7a08d7ed14dbe48dd56d91e7f78b097
TammushB <tammy.biniashvili@ultimagen.com>
2025-05-27 08:07:21 +0000

    rename flag to include both losses and gains

--- R/cn.mops.R
+++ R/cn.mops.R
@@ -249,7 +249,7 @@ cn.mops <- function(input,I = c(0.025,0.5,1,1.5,2,2.5,3,3.5,4),
 		norm=1, normType="poisson",sizeFactor="mean",normQu=0.25, quSizeFactor=0.75,
 		upperThreshold=0.5,lowerThreshold=-0.9,
 		minWidth=3,segAlgorithm="fast",minReadCount=5,useMedian=FALSE,
-		returnPosterior=FALSE,moderate_amplifications=FALSE,...){
+		returnPosterior=FALSE,mod_cnv=FALSE,...){
 	
 	############ check input ##################################################
 	if(any(class(input)=="GRanges")){
@@ -566,7 +566,7 @@ cn.mops <- function(input,I = c(0.025,0.5,1,1.5,2,2.5,3,3.5,4),
 				chrIdx <- chrDf[chrom,1]:chrDf[chrom,2]
 				
 				if (parallel==0){
-					if(moderate_amplifications){
+					if(mod_cnv){
 						resSegmList[[chrom]] <- apply(sINI[chrIdx, ,drop=FALSE],2,
 							segment,
 							minSeg=minWidth,
@@ -580,7 +580,7 @@ cn.mops <- function(input,I = c(0.025,0.5,1,1.5,2,2.5,3,3.5,4),
 					}
 				} else {
 					
-					if(moderate_amplifications){
+					if(mod_cnv){
 						cl <- parallel::makeCluster(as.integer(parallel),type="SOCK")
 						parallel::clusterEvalQ(cl,"segment")
 						resSegmList[[chrom]] <- parallel::parApply(cl,sINI[chrIdx, ,drop=FALSE],2,
